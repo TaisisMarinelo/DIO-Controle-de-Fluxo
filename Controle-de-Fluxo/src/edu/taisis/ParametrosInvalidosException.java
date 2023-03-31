4dd2d42ab0da0e2872fa7d9d7c2b18638ce44889
TaisisMarinelo <taisismarinelo@gmail.com>
2023-03-31 19:52:55 -0300

    upload project control;

---
+++ Controle-de-Fluxo/src/edu/taisis/ParametrosInvalidosException.java
@@ -0,0 +1,16 @@
+package edu.taisis;
+
+public class ParametrosInvalidosException extends Exception{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getMessage() {
+		return "O Primeiro parâmetro e maior que o segundo parâmetro.";
+		
+	}
+
+}
